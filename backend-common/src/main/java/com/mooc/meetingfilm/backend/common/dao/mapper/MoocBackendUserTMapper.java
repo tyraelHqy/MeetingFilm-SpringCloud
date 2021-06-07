366c117496dae9f673dd82181202d0834c95111d
tyraelHqy <752546894@qq.com>
2021-06-07 16:26:22 +0800

    完成Mybatis-plus配置，以及代码生成

---
+++ backend-common/src/main/java/com/mooc/meetingfilm/backend/common/dao/mapper/MoocBackendUserTMapper.java
@@ -0,0 +1,16 @@
+package com.mooc.meetingfilm.backend.common.dao.mapper;
+
+import com.mooc.meetingfilm.backend.common.dao.entity.MoocBackendUserT;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 后台用户表 Mapper 接口
+ * </p>
+ *
+ * @author tyrael
+ * @since 2021-06-07
+ */
+public interface MoocBackendUserTMapper extends BaseMapper<MoocBackendUserT> {
+
+}
