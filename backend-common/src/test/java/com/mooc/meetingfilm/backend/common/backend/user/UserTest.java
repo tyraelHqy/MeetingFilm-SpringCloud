8592ec751a97d36b3a7563c6ea4a57607b33d447
tyraelHqy <752546894@qq.com>
2021-06-07 23:48:48 +0800

    完成Mybatis-plus 高级特性之条件封装

--- backend-common/src/test/java/com/mooc/meetingfilm/backend/common/backend/user/UserTest.java
+++ backend-common/src/test/java/com/mooc/meetingfilm/backend/common/backend/user/UserTest.java
@@ -1,5 +1,7 @@
 package com.mooc.meetingfilm.backend.common.backend.user;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.mooc.meetingfilm.backend.common.BackendCommonApplication;
 import com.mooc.meetingfilm.backend.common.BackendCommonApplicationTests;
 import com.mooc.meetingfilm.backend.common.dao.entity.MoocBackendUserT;
@@ -16,23 +18,38 @@ public class UserTest extends BackendCommonApplicationTests {
 
     @Test
     public void add(){
-        MoocBackendUserT user = new MoocBackendUserT();
-        user.setUserName("admin");
-        user.setUserPwd("admin");
-        user.setUserPhone("13100000000");
-
-        backendUser.insert(user);
+        for (int i = 0; i < 5; i++) {
+            MoocBackendUserT user = new MoocBackendUserT();
+            user.setUserName("admin"+i);
+            user.setUserPwd("admin"+i);
+            user.setUserPhone("1310000000"+i);
+            backendUser.insert(user);
+        }
+//        MoocBackendUserT user = new MoocBackendUserT();
+//        user.setUserName("admin");
+//        user.setUserPwd("admin");
+//        user.setUserPhone("13100000000");
+//
+//        backendUser.insert(user);
     }
 
     @Test
     public void select(){
         // 根据主键查询
-        MoocBackendUserT moocBackendUserT = backendUser.selectById(2);
-        System.out.println("moocBackendUserT: "+moocBackendUserT);
+//        MoocBackendUserT moocBackendUserT = backendUser.selectById(2);
+//        System.out.println("moocBackendUserT: "+moocBackendUserT);
+//
+//        // 查询列表
+//        List<MoocBackendUserT> users = backendUser.selectList(null);
+//        users.stream().forEach(
+//                System.out::println
+//        );
 
-        // 查询列表
-        List<MoocBackendUserT> users = backendUser.selectList(null);
-        users.stream().forEach(
+        // 查询列表带条件
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.like("user_name","admin");
+        List<MoocBackendUserT> user = backendUser.selectList(queryWrapper);
+        user.stream().forEach(
                 System.out::println
         );
     }
@@ -45,7 +62,17 @@ public class UserTest extends BackendCommonApplicationTests {
         user.setUserPwd("Tyrael");
         user.setUserPhone("18511110000");
 
+        // 根据主键修改
         backendUser.updateById(user);
+
+        // 根据条件修改
+        QueryWrapper<MoocBackendUserT> wrapper = new QueryWrapper<>();
+        wrapper.eq("user_name","admin4");
+        MoocBackendUserT user2 = new MoocBackendUserT();
+        user2.setUserName("admin");
+        user2.setUserPwd("Tyrael");
+        user2.setUserPhone("18511110000");
+        int update = backendUser.update(user2, wrapper);
     }
 
     @Test
