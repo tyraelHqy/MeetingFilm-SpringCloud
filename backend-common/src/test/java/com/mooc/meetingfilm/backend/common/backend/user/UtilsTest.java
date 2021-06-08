0c1ef158087673b7e5b47783e1e96d2f73b2703c
tyraelHqy <752546894@qq.com>
2021-06-08 15:44:46 +0800

    完成utils模块搭建，更新common模块中的依赖以及parent模块中的依赖

---
+++ backend-common/src/test/java/com/mooc/meetingfilm/backend/common/backend/user/UtilsTest.java
@@ -0,0 +1,12 @@
+package com.mooc.meetingfilm.backend.common.backend.user;
+
+import com.mooc.meetingfilm.utils.common.vo.BaseResponseVO;
+import org.junit.Test;
+
+public class UtilsTest {
+    @Test
+    public void test(){
+        BaseResponseVO responseVO = new BaseResponseVO();
+        System.out.println(responseVO.run("Hello World"));
+    }
+}
---
+++ backend-utils/src/main/java/com/mooc/meetingfilm/utils/common/vo/BaseResponseVO.java
@@ -0,0 +1,12 @@
+package com.mooc.meetingfilm.utils.common.vo;
+
+/**
+ * 表现层公共返回
+ */
+public class BaseResponseVO {
+
+    public String run(String message){
+        return "baseResponseVO: "+message;
+
+    }
+}
