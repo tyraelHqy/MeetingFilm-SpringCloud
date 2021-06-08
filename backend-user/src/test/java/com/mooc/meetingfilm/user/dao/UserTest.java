33cea47b3fb958436543a61cb755ae2c957d82e5
tyraelHqy <752546894@qq.com>
2021-06-08 20:52:54 +0800

    新增MD5加密

--- backend-user/src/test/java/com/mooc/meetingfilm/user/dao/UserTest.java
+++ backend-user/src/test/java/com/mooc/meetingfilm/user/dao/UserTest.java
@@ -3,6 +3,7 @@ package com.mooc.meetingfilm.user.dao;
 import com.mooc.meetingfilm.user.BackendUserApplicationTests;
 import com.mooc.meetingfilm.user.dao.entity.MoocBackendUserT;
 import com.mooc.meetingfilm.user.dao.mapper.MoocBackendUserTMapper;
+import com.mooc.meetingfilm.utils.util.MD5Util;
 import org.junit.Test;
 
 import javax.annotation.Resource;
@@ -14,9 +15,18 @@ public class UserTest extends BackendUserApplicationTests {
     @Resource
     public MoocBackendUserTMapper backendUser;
 
+    /**
+     * 添加后台运维用户功能
+     */
     @Test
     public void add(){
 
+        MoocBackendUserT user = new MoocBackendUserT();
+        user.setUserName("admin");
+        user.setUserPwd(MD5Util.encrypt("admin123"));
+        user.setUserPhone("18088888888");
+
+        backendUser.insert(user);
     }
 
     @Test
