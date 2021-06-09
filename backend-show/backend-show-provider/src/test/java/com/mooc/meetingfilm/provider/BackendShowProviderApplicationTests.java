bfa0f157a6ea3e811a0197d88cd325f5392d41a1
tyraelHqy <752546894@qq.com>
2021-06-09 18:56:32 +0800

    公共演示环境搭建完成

---
+++ backend-show/backend-show-provider/src/test/java/com/mooc/meetingfilm/provider/BackendShowProviderApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mooc.meetingfilm.provider;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class BackendShowProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
