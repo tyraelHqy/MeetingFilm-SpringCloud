2630dbd56a156186ca336e7e48cab2e915ae6923
tyraelHqy <752546894@qq.com>
2021-06-10 17:58:07 +0800

    播放厅模块框架搭建

---
+++ backend-hall/src/main/java/com/mooc/meetingfilm/hall/dao/mapper/MoocFieldTMapper.java
@@ -0,0 +1,16 @@
+package com.mooc.meetingfilm.hall.dao.mapper;
+
+import com.mooc.meetingfilm.hall.dao.entity.MoocFieldT;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 放映场次表 Mapper 接口
+ * </p>
+ *
+ * @author tyrael
+ * @since 2021-06-10
+ */
+public interface MoocFieldTMapper extends BaseMapper<MoocFieldT> {
+
+}
