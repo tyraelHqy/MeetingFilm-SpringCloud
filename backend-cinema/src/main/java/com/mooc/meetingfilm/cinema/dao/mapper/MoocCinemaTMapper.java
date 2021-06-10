6801e9b5bc5671eaf02242fe93a106479569ff76
tyraelHqy <752546894@qq.com>
2021-06-10 16:10:57 +0800

    影院模块环境搭建

---
+++ backend-cinema/src/main/java/com/mooc/meetingfilm/cinema/dao/mapper/MoocCinemaTMapper.java
@@ -0,0 +1,16 @@
+package com.mooc.meetingfilm.cinema.dao.mapper;
+
+import com.mooc.meetingfilm.cinema.dao.entity.MoocCinemaT;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影院信息表 Mapper 接口
+ * </p>
+ *
+ * @author tyrael
+ * @since 2021-06-10
+ */
+public interface MoocCinemaTMapper extends BaseMapper<MoocCinemaT> {
+
+}
