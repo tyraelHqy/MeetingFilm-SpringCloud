d2fda14441099cdaea8318cfa2ddce437e7ec154
tyraelHqy <752546894@qq.com>
2021-06-21 14:45:49 +0800

    Hystrix 实战完成

--- backend-cinema/src/main/java/com/mooc/meetingfilm/cinema/BackendCinemaApplication.java
+++ backend-cinema/src/main/java/com/mooc/meetingfilm/cinema/BackendCinemaApplication.java
@@ -4,10 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
 @MapperScan(basePackages = {"com.mooc.meetingfilm.cinema.dao.mapper"})
 @ComponentScan(basePackages = {"com.mooc.meetingfilm"})
 public class BackendCinemaApplication {
